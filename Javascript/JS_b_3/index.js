6fffdb714c99da2d076eb4c652b60f4b2512056a
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-01-17 21:11:55 +0530

    new

--- Javascript/JS_b_3/index.js
+++ Javascript/JS_b_3/index.js
@@ -3,3 +3,9 @@ console.log("Hello Jee");
 let lastName = 'Babbar';
 
 let firstName = new String('Love');
+
+let message = 'This is my message';
+
+let words = message.split(' ');
+
+console.log(words);
