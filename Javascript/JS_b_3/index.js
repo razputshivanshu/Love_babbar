8a39e8f762139edfd3e52fcd24c72f58a08c2cc4
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-01-19 10:37:58 +0530

    s

--- Javascript/JS_b_3/index.js
+++ Javascript/JS_b_3/index.js
@@ -24,30 +24,90 @@
 // console.log(date2);
 
 
-let numbers = [1,4,5,7];
+// let numbers = [1,4,5,7];
 
-console.log(numbers[0]);
+// console.log(numbers[0]);
 
-//push at the right
-numbers.push(9);
+// //push at the right
+// numbers.push(9);
 
-//push at the left
-numbers.unshift(10);
+// //push at the left
+// numbers.unshift(10);
 
-numbers.splice(2,0,'a','b','c');
+// numbers.splice(2,0,'a','b','c');
 
+// console.log(numbers);
+
+// console.log(numbers.indexOf('a'));
+
+// console.log(numbers.indexOf('z'));
+
+// //if we want to check if a number exist in an array
+
+// if(numbers.indexOf[4] != -1){
+//   console.log("present");
+// }
+
+// //but this is not a good way better way is to use includes.
+
+// console.log(numbers.includes(7));
+
+
+// let courses = [
+//   {no:1, naam:'Shiv'},
+//   {no:2, naam:'Love'}
+// ]
+
+// console.log(courses);
+
+
+// console.log(courses.indexOf( {no:1,naam:'Love'} ));
+
+// console.log(courses.includes( {no:1,naam:'Love'} ));
+
+//These functions doesn't work with objects because both these objects are different and have different references.
+
+
+// let course= courses.find(function(course){
+//   return course.naam == 'Love';
+// });
+// console.log(course);
+// to make it more smaller we are going to use arrow function
+
+
+// let course = courses.find(course => course.naam ==='Love');
+// console.log(course);
+
+// let number = courses.find(number => number.find == 1);
+// console.log(number);
+
+
+
+//Removing Element
+
+//end - pop
+//begin - shift
+//middle - splice
+
+// let numbers = [1,2,3,4,5,6,7];
+
+// numbers.pop();
+// console.log(numbers);
+
+// numbers.shift();
+// console.log(numbers);
+
+// numbers.splice(2,1); //(index,how many)
+// console.log(numbers);
+
+
+
+
+//// Emptying an Array
+
+let numbers =[1,2,3,5,6];
+let numbers2=numbers;
+
+numbers = [];
 console.log(numbers);
-
-console.log(numbers.indexOf('a'));
-
-console.log(numbers.indexOf('z'));
-
-//if we want to check if a number exist in an array
-
-if(numbers.indexOf[4] != -1){
-  console.log("present");
-}
-
-//but this is not a good way better way is to use includes.
-
-console.log(numbers.includes(7));
+console.log(numbers2);
