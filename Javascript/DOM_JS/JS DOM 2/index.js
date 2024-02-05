25835656c51f710f9d7601fe2135adc6b8af3d4f
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-02-05 11:17:53 +0530

    com

--- Javascript/DOM_JS/JS DOM 2/index.js
+++ Javascript/DOM_JS/JS DOM 2/index.js
@@ -16,19 +16,30 @@
 // first.addEventListener('click',function(event){event.preventDefault();
 // console.log("mazza aaya");});
 
-let myDiv = document.createElement('div');
-
-for(let i =1;i<=100;i++){
-  let newElement= document.createElement('p');
-  newElement.textContent= 'This is para'+i;
+// let myDiv = document.createElement('div');
 
 
-  newElement.addEventListener('click', function(event){console.log('I have clicked on paragraph');});
+// function ev(event){
+//   console.log('I have clicked on paragraph' + event.target.textContent);
+// }
 
-  myDiv.appendChild(newElement);
+// myDiv.addEventListener('click', ev);
 
-}
-document.body.appendChild(myDiv);
+// for(let i =1;i<=100;i++){
+//   let newElement= document.createElement('p');
+//   newElement.textContent= 'This is para'+i;
 
 
+  
+
+//   myDiv.appendChild(newElement);
+
+// }
+// document.body.appendChild(myDiv);
+
+// -----
+let element = document.querySelector('#wrapper');
+
+element.addEventListener('click',function(event){console.log('span pr click kiya hai' + event.target.textContent);
+})
 
