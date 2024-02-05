45faa133c2d8e33ee7be26911cd04bfce940c70a
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-02-05 11:21:52 +0530

    nodeName

--- Javascript/DOM_JS/JS DOM 2/index.js
+++ Javascript/DOM_JS/JS DOM 2/index.js
@@ -40,6 +40,9 @@
 // -----
 let element = document.querySelector('#wrapper');
 
-element.addEventListener('click',function(event){console.log('span pr click kiya hai' + event.target.textContent);
-})
+element.addEventListener('click',function(event){
+  if(event.target.nodeName === 'SPAN'){
+  console.log('span pr click kiya hai' + event.target.textContent);
+       }
+});
 
