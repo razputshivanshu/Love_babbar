6ba792c8eee215173d348a674d7f1d87cf132b9d
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-02-05 10:43:15 +0530

    dom

--- Javascript/DOM_JS/JS DOM 2/index.js
+++ Javascript/DOM_JS/JS DOM 2/index.js
@@ -1,5 +1,12 @@
 function eventL(){console.log("I am moving towards my goals \n Software Engineer at Google");}
 
-document.addEventListener('click',eventL);
+// document.addEventListener('click',eventL);
 
-// document.removeEventListener('click',eventL);
+// 
+const content = document.querySelector('#wrapper');
+
+content.addEventListener('click', function(event){
+  console.log("I can do everything\n", event);
+});
+
+
