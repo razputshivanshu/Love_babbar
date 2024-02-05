ad3a3b65f8493c3a61cb47f8a1863af1a96ee13a
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-02-05 10:51:28 +0530

    hjk

--- Javascript/DOM_JS/JS DOM 2/index.js
+++ Javascript/DOM_JS/JS DOM 2/index.js
@@ -1,12 +1,21 @@
-function eventL(){console.log("I am moving towards my goals \n Software Engineer at Google");}
+// function eventL(){console.log("I am moving towards my goals \n Software Engineer at Google");}
 
-// document.addEventListener('click',eventL);
+// // document.addEventListener('click',eventL);
 
-// 
-const content = document.querySelector('#wrapper');
+// // 
+// const content = document.querySelector('#wrapper');
 
-content.addEventListener('click', function(event){
-  console.log("I can do everything\n", event);
-});
+// content.addEventListener('click', function(event){
+//   console.log("I can do everything\n", event);
+// });
+
+// let xyz= document.querySelectorAll('a');
+
+// let first = xyz[0];
+
+// first.addEventListener('click',function(event){event.preventDefault();
+// console.log("mazza aaya");});
+
+
 
 
