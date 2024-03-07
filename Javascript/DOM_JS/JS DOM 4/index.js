206fa947329f2a67549fda24d8282ce998becebd
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-03-07 19:33:27 +0530

    done

--- Javascript/DOM_JS/JS DOM 4/index.js
+++ Javascript/DOM_JS/JS DOM 4/index.js
@@ -125,3 +125,46 @@
 // }
 
 
+//Fetch API : I already know this and I can understand more deeply, it is widely used in express js.
+
+
+// async function utility(){
+// let content = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+// let output = await content.json();
+
+// console.log(output);
+// }
+
+
+async function helper(){
+
+
+let options = {
+  method: 'POST',
+  body: JSON.stringify({
+  title: 'foo', body: 'bar', userId: 1,
+  }),
+  headers:{
+  'Content-type': 'application/json; charset=UTF-8'}
+};
+
+
+  let content = await fetch ('https://jsonplaceholder.typicode.com/posts',options);
+
+  let response = content.json();
+
+  return response;
+
+}
+
+async function utility(){
+  let ans = await helper();
+  console.log(helper);
+}
+
+utility();
+
+
+ 
+
