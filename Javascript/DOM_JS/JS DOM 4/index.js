754378caec8e1bcd114d5cecd39948010886bb0a
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-03-07 18:28:44 +0530

    simp

--- Javascript/DOM_JS/JS DOM 4/index.js
+++ Javascript/DOM_JS/JS DOM 4/index.js
@@ -1,13 +1,26 @@
 
 
-setTimeout(function(){
-  console.log('third');
-},3000);
+// setTimeout(function(){
+//   console.log('third');
+// },3000);
 
-function sync(){
-  console.log('first');
+// function sync(){
+//   console.log('first');
 
-}
-sync();
-console.log('second');
+// }
+// sync();
+// console.log('second');
+
+
+let meraPromise = new Promise(function(resolve,reject){
+  // console.log("I am inside promise");
+  // resolve(1998);
+
+  setTimeout(function(){
+    console.log("I am on the way to become the world best software developer");
+    resolve(2233);
+  }, 5000);
+});   // in a promise two parameters can be sent resolve or reject
+
+console.log('Pehla');
 
