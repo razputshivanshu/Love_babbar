12d65d6ccff16a7997218d4caffaa0d1d46cd445
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-05-31 22:56:30 +0530

    gg

---
+++ Javascript/Minor Projects/Tic Tac Toe/index.js
@@ -0,0 +1,49 @@
+
+
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [1,4,7],
+  [0,3,6],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+
+]
+
+// let create a function to initialize the game
+
+function initGame(){
+  currentPlayer = "X";
+  gameGrid = ["","","","","","","","",""];
+  newGameBtn.classList.remove("active");
+
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
+
+}
+
+initGame();
+
+
+//Hrr box prr event listener laga dia
+boxes.forEach((box,index) =>{
+  box.addEventListener("click",()=>{
+    handleClick(index);
+  });
+
+});
+
+
+
+
+
