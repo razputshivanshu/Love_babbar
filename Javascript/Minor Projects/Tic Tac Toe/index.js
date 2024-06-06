3653f6874efa6254a03a8980bf7b524b3c48e6af
razputshivanshu <89507255+razputshivanshu@users.noreply.github.com>
2024-06-06 11:11:39 +0530

    React

--- Javascript/Minor Projects/Tic Tac Toe/index.js
+++ Javascript/Minor Projects/Tic Tac Toe/index.js
@@ -25,23 +25,124 @@ const winningPositions = [
 function initGame(){
   currentPlayer = "X";
   gameGrid = ["","","","","","","","",""];
+
+  //UI prr b empty krna pdega
+  boxes.forEach((box,index)=>{
+    box.innerText = "";
+  })
   newGameBtn.classList.remove("active");
 
   gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
 
 
 }
 
 initGame();
 
+//Main function
+
+function checkGameOver(){
+  //To DO
+  let answer = "";
+
+  winningPositions.forEach((positions)=>{
+
+    //all 3 boxes are non empty and are in same in value
+    if((gameGrid[positions[0]] !== "" || gameGrid[positions[1]] !== "" || gameGrid[positions[2]] !== "" ) && (gameGrid[positions[0]] == gameGrid[positions[1]])&& gameGrid[positions[0]] == gameGrid[positions[2]]){
+
+      //check is winner is X
+      if(gameGrid[positions[0]] ==="X"){
+        answer = "X";
+      }
+      else{
+        answer = "O";
+      }
+      //disable pointer event
+      boxes.forEach((box)=>{
+        box.style.pointerEvents = "none";
+      })
+
+      //now we know who won so print the color
+      boxes[positions[0]].classList.add("win");
+      boxes[positions[1]].classList.add("win");
+      boxes[positions[2]].classList.add("win");
+
+    }
+  });
+  // it means we have a winner;
+  if(answer !== ""){
+    gameInfo.innerText = `Winner Player - ${answer}`;
+    newGameBtn.classList.add("active");
+    return;
+  }
+
+  //else it is a tie
+  //check the whole board if it is fill
+  let fillCount = 0;
+  gameGrid.forEach((box)=>{
+    if(box !== "")
+    fillCount++;
+  });
+
+  if(fillCount === 9){
+    gameInfo.innerText = "Game Tied";
+    newGameBtn.classList.add("active");
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function swapTurn(){
+  if(currentPlayer==="X"){
+    currentPlayer ="O";
+  }
+  else{
+    currentPlayer = "X";
+  }
+  //UI update krdo
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+
+
+function handleClick(index){
+  if(gameGrid[index] === ""){
+    boxes[index].innerText = currentPlayer;
+    gameGrid[index] = currentPlayer;
+
+    //swap kro turn ko
+    swapTurn();
+    //check kro ki koi jeet to nii gya
+    checkGameOver();
+  }
+}
+
 
 //Hrr box prr event listener laga dia
 boxes.forEach((box,index) =>{
   box.addEventListener("click",()=>{
     handleClick(index);
-  });
+  })
 
 });
+
+
+
+newGameBtn.addEventListener("click", initGame);
 
 
 
